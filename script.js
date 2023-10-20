07f0a77183b93553004fb63af800b02b1b9f22e5
elia <eliaotito@eastafricaknxsolution.com>
2023-10-20 19:57:07 -0400

    updating my project

--- script.js
+++ script.js
@@ -9,16 +9,37 @@ async function fetchData(){
 }
 // fetchData()
 
+async function getFirstPost(){
+    let getdata = fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLEVkQGIATCXJzEBw0AinG_WsGNmwspPPr&key=AIzaSyA6aG6XTAj5ktnvySRT_2nwTna45arwZ2s")
+    let getresp = (await getdata).json()
+    let showdata = await getresp
+    let showresult = showdata.items
+    return showresult
+}
+
+
+
+// async function displayFirstPost(){
+//     let fetchdata= await getFirstPost()
+//     // console.log(fetchData)
+//     // let mainnvav = document.querySelector('.')
+
+// }
+
+// displayFirstPost()
 
 async function displayCart (){
-    let fetchitems= await fetchData() // async 
+    let fetchitems= await getFirstPost() // async
+
     console.log(fetchitems)
-    // console.log(fData[0].snippet.thumbnails.default)
+    // console.log(fetchitems[0].snippet.resourceId)
     let videoCard = document.querySelector(".main-content")
     let result_items =fetchitems.map(element => {
         let video = `<div class="video-card">
                         <div class="videoimg">
-                            <img  src=${element.snippet.thumbnails.medium.url} alt="" class="src">
+                             <a target="_blank" href="https://www.youtube.com/watch?v=${element.snippet.resourceId.videoId}">
+                                <img  src=${element.snippet.thumbnails.medium.url} alt="" class="src">
+                            </a>
                      </div>
                 <div class="card-informa">
                     <div class="channelimg">
