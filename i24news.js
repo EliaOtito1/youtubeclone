25a153296bbbcd244c4af547fac259140fa5464c
elia <eliaotito@eastafricaknxsolution.com>
2023-10-26 21:03:55 -0400

    Adding new items

---
+++ i24news.js
@@ -0,0 +1,45 @@
+async function i24News(){
+    let getData = fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=PLxnSgwbAmIteZuwUiyg2yB8Jo8G56HvRd&key=AIzaSyA6aG6XTAj5ktnvySRT_2nwTna45arwZ2s")
+    let response = (await getData).json();
+    let objData = await response
+    let result = await objData.items;
+    console.log(result)
+    return result;
+   
+}
+
+// ryanFetch()
+
+async function display24() {
+    let getRyan = await i24News()
+    // console.log(getRyan)
+    let videoCard = document.querySelector(".main-content")
+    let showItems = getRyan.map(element => {
+        let video = `<div class="video-card">
+        <div class="videoimg">
+            <a target="_blank" href="https://www.youtube.com/watch?v=${element.snippet.resourceId.videoId}">
+                <img src=${element.snippet.thumbnails.medium.url} alt="" class="src">
+            </a>   
+       </div>
+
+       <div class="card-informa">
+           <div class="channelimg">
+               <img src="" alt="" class="src">
+           </div>
+
+           <div class="desc">
+           <a target="_blank" href="https://www.youtube.com/watch?v=${element.snippet.resourceId.videoId}">
+               <div class="videoname">${element.snippet.title}</div>
+               <div class="channelname">${element.snippet.channelTitle}</div>
+            </a>   
+               <div class="channelviewers"><span>13.1M subscribers</span></div>
+           </div>
+        </div>
+        </div>`
+        return video
+    })
+videoCard.innerHTML=showItems.join("")
+   // console.log(getRyan)
+}
+
+display24()
---
+++ ourjews.js
@@ -0,0 +1,45 @@
+async function ourJews(){
+    let getData = fetch("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLBPDdJBXRyTMlqSzI7o1jTbAhM2BgQRsm&key=AIzaSyA6aG6XTAj5ktnvySRT_2nwTna45arwZ2s")
+    let response = (await getData).json();
+    let objData = await response
+    let result = await objData.items;
+    // console.log(result)
+    return result;
+   
+}
+
+// ryanFetch()
+
+async function displayJews() {
+    let getRyan = await ourJews()
+    // console.log(getRyan)
+    let videoCard = document.querySelector(".main-content")
+    let showItems = getRyan.map(element => {
+        let video = `<div class="video-card">
+        <div class="videoimg">
+            <a target="_blank" href="https://www.youtube.com/watch?v=${element.snippet.resourceId.videoId}">
+                <img src=${element.snippet.thumbnails.medium.url} alt="" class="src">
+            </a>   
+       </div>
+
+       <div class="card-informa">
+           <div class="channelimg">
+               <img src="" alt="" class="src">
+           </div>
+
+           <div class="desc">
+           <a target="_blank" href="https://www.youtube.com/watch?v=${element.snippet.resourceId.videoId}">
+               <div class="videoname">${element.snippet.title}</div>
+               <div class="channelname">${element.snippet.channelTitle}</div>
+            </a>   
+               <div class="channelviewers"><span>13.1M subscribers</span></div>
+           </div>
+        </div>
+        </div>`
+        return video
+    })
+videoCard.innerHTML=showItems.join("")
+   // console.log(getRyan)
+}
+
+displayJews()
